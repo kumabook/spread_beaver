4f510c87aff8048843a2e58b0086fb814210a040
Hiroki Kumamoto <kumabook@live.jp>
2016-01-31 22:58:31 +0900

    Add track controller and view

---
+++ app/controllers/tracks_controller.rb
@@ -0,0 +1,64 @@
+class TracksController < ApplicationController
+  before_action :set_track, only: [:show, :update, :destroy]
+  before_action :set_entry, only: [:index]
+
+  # GET /tracks
+  # GET /tracks.json
+  def index
+    if @entry.present?
+      @tracks = @entry.tracks
+    else
+      @tracks = Track.all
+    end
+  end
+
+  # GET /tracks/1
+  # GET /tracks/1.json
+  def show
+  end
+
+  # POST /tracks
+  # POST /tracks.json
+  def create
+    @track = Track.new(track_params)
+
+    if @track.save
+      format.html { redirect_to tracks_path, notice: 'Track was successfully created.' }
+    else
+      format.html { render :new }
+    end
+  end
+
+  # PATCH/PUT /tracks/1
+  # PATCH/PUT /tracks/1.json
+  def update
+    if @track.update(track_params)
+      format.html { redirect_to tracks_path, notice: 'Track was successfully updated.' }
+    else
+      format.html { render :edit }
+    end
+  end
+
+  # DELETE /tracks/1
+  # DELETE /tracks/1.json
+  def destroy
+    @track.destroy
+    respond_to do |format|
+      format.html { redirect_to tracks_path, notice: 'Track was successfully destroyed.' }
+    end
+  end
+
+  private
+
+    def set_track
+      @track = Track.find(params[:id])
+    end
+
+    def set_entry
+      @entry = Entry.find(params[:entry_id])
+    end
+
+    def track_params
+      params.require(:track).permit(:identifier, :provider, :title, :url)
+    end
+end
---
+++ app/views/tracks/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @track, :id, :identifier, :provider, :title, :url, :created_at, :updated_at
