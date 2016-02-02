4f5c45f66998deb838d3f348d321cca4b237ce68
Hiroki Kumamoto <kumabook@live.jp>
2016-02-03 00:51:17 +0900

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,21 @@
 Rails.application.routes.draw do
+  feed_id_regex  = /[a-zA-Z1-9\.%#\$&\?\(\)\=\+\-\:\?\\]+/
+  entry_id_regex = /[a-zA-Z1-9\-]+/
   use_doorkeeper
   root :to => 'feeds#index'
   resources :user_sessions
-  resources :users
-  resources :subscriptions, only: [:index]
-
-  feed_id_regex = /[a-zA-Z1-9\.%#\$&\?\(\)\=\+\-\:\?\\]+/
-  entry_id_regex = /[a-zA-Z1-9\-]+/
+  resources :users do
+    resources :entries, only: [:index], constraints: { id: entry_id_regex }
+  end
+  resources :entries do
+    resources :tracks, only: :index
+  end
   resources :feeds, constraints: { id: feed_id_regex },
                     shallow: true do
     resources :entries, only: [:index], constraints: { id: entry_id_regex }
   end
-  resources :entries
+  resources :subscriptions, only: [:index, :create, :destroy]
+  resources :tracks
 
   get 'login' => 'user_sessions#new', :as => :login
   post 'logout' => 'user_sessions#destroy', :as => :logout
