fec9960f46c000183b164985d35594a686155817
Hiroki Kumamoto <kumabook@live.jp>
2016-02-23 01:05:08 +0900

    Don't expire access_token

--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -24,6 +24,7 @@ Doorkeeper.configure do
 
   # Access token expiration time (default 2 hours).
   # If you want to disable expiration, set this to nil.
+  access_token_expires_in nil
   # access_token_expires_in 2.hours
 
   # Assign a custom TTL for implicit grants.
