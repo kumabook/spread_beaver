d3536682ea0f4da4e3276ad12040cb769d685848
Hiroki Kumamoto <kumabook@live.jp>
2016-02-23 00:59:22 +0900

    Add ApiController

---
+++ app/controllers/api/v1/api_controller.rb
@@ -0,0 +1,7 @@
+class Api::V1::ApiController < ActionController::API
+  include ActionController::Serialization
+#  protect_from_forgery with: :null_session
+  def current_resource_owner
+    User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
+  end
+end
