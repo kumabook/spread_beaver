ecfef9127d611ba17004d0d1bde305f6c3389fbe
Hiroki Kumamoto <kumabook@live.jp>
2016-02-23 01:03:14 +0900

    Add credentials api spec

---
+++ spec/api/v1/credentials_api_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe "Credentials api", :type => :request do
+  before(:all) do
+    setup()
+    login()
+  end
+
+  it "displays me" do
+    get "/api/v1/me", nil, Authorization: "Bearer #{@token['access_token']}"
+    me = JSON.parse @response.body
+    expect(me['id']).to    eq(@user.id)
+    expect(me['email']).to eq(@user.email)
+  end
+end
