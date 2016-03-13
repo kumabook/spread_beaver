3d02d1c682fd2677d565ca8f16a20d0bd66ecba6
Hiroki Kumamoto <kumabook@live.jp>
2016-03-13 23:35:41 +0900

    Add users property to show track api `GET /v3/tracks/:id`

--- app/controllers/v3/tracks_controller.rb
+++ app/controllers/v3/tracks_controller.rb
@@ -4,13 +4,13 @@ class V3::TracksController < V3::ApiController
 
   def show
     if @track.present?
-      render json: @track.to_json, status: 200
+      render json: @track.as_json(include: :users), status: 200
     else
       render json: {}, status: :not_found
     end
   end
 
   def set_track
-    @track = Track.find(params[:id])
+    @track = Track.includes(:users).find(params[:id])
   end
 end
