56cb388c6f125e0df34b1b6240565f234edb06ff
Hiroki Kumamoto <kumabook@live.jp>
2016-04-10 13:29:38 +0900

    Make Feed#topics editable from html view

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -61,6 +61,6 @@ class FeedsController < ApplicationController
   end
 
   def feed_params
-    params.require(:feed).permit(:id, :title, :description, :website)
+    params.require(:feed).permit(:id, :title, :description, :website, :topics)
   end
 end
