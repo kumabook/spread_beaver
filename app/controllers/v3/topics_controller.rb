e551eb33a7aa9bf8e3607391f38eac02d48016d8
Hiroki Kumamoto <kumabook@live.jp>
2016-04-29 10:48:54 +0900

    Add topic api controller

---
+++ app/controllers/v3/topics_controller.rb
@@ -0,0 +1,32 @@
+class V3::TopicsController < V3::ApiController
+  before_action :doorkeeper_authorize!
+  before_action :set_topic,  except: [:index]
+
+  def index
+    @topic = Topic.order('engagement DESC').all
+    render json: @topic.to_json, status: 200
+  end
+
+  def update
+    if @topic.update(label: params[:label],
+                     description: params[:description])
+      render json: @topic.to_json, status: 200
+    else
+      render json: {}, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    if @topic.nil?
+      render json: {}, status: :not_found
+    elsif @topic.destroy
+      render json: {}, status: 200
+    else
+      render json: {}, status: :unprocessable_entity
+    end
+  end
+
+  def set_topic
+    @topic = Topic.find(params[:id])
+  end
+end
