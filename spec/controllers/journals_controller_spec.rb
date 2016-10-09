8236bd96c6aa80a8ffc82058c3340e28ad402ff2
Hiroki Kumamoto <kumabook@live.jp>
2016-10-09 12:40:59 +0900

    Add journal controller spec

---
+++ spec/controllers/journals_controller_spec.rb
@@ -0,0 +1,47 @@
+require 'rails_helper'
+
+describe JournalsController, type: :controller do
+  let! (:journal) {     Journal.create!(label: "journal", description: "desc")}
+  let  (:user   ) { FactoryGirl.create (:admin                               )}
+
+  before(:each) do
+    login_user user
+  end
+
+  describe 'GET index' do
+    before { get :index }
+    it { expect(assigns(:journals)).to eq([journal])  }
+    it { expect(response).to render_template("index") }
+  end
+
+  describe 'GET new' do
+    before { get :new }
+    it { expect(response).to render_template("new") }
+  end
+
+  describe 'POST create' do
+    label       = "new_journal"
+    description = "desc"
+    before { post :create, journal: { label: label, description: description} }
+    it { expect(response).to redirect_to journals_url }
+    it { expect(Journal.find_by(label: label).label).to eq(label) }
+  end
+
+  describe 'GET edit' do
+    before { get :edit, id: journal.id }
+    it { expect(response).to render_template("edit") }
+  end
+
+  describe 'POST update' do
+    label = "changed"
+    before { post :update, id: journal.id, journal: { label: label } }
+    it { expect(response).to redirect_to journals_url }
+    it { expect(Journal.find(journal.id).label).to eq(label) }
+  end
+
+  describe "DELETE destroy" do
+    before { delete :destroy, id: journal.id }
+    it { expect(response).to redirect_to journals_url }
+    it { expect(Journal.find_by(id: journal.id)).to be_nil }
+  end
+end
