847ae0804afbb496022cec13696247dad8e035c6
Hiroki Kumamoto <kumabook@live.jp>
2016-10-10 13:25:29 +0900

    Use sorcery helper for controller spec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -11,12 +11,11 @@ Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
 
   config.filter_rails_from_backtrace!
+  config.include Sorcery::TestHelpers::Rails::Controller, type: :controller
   config.include ApiMacros, :type => :request
 end
 
