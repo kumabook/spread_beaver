9c4595bee67c8af845a7489c6bac4896796ee633
Hiroki Kumamoto <kumabook@live.jp>
2016-10-29 00:38:41 +0900

    Fix bugs of subscription dependent. Don't destroy user and feed

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,6 +1,6 @@
 class Subscription < ActiveRecord::Base
-  belongs_to :user                   , dependent: :destroy
-  belongs_to :feed                   , dependent: :destroy
+  belongs_to :user
+  belongs_to :feed
   has_many   :subscription_categories, dependent: :destroy
   has_many   :categories             , through: :subscription_categories
 end
