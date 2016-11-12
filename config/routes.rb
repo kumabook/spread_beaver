b1e9801055b28a6805e6ca823fc6fb45202270b9
Hiroki Kumamoto <kumabook@live.jp>
2016-11-12 11:05:28 +0900

    Fix routes.rb: Add empty string for POST update api

--- config/routes.rb
+++ config/routes.rb
@@ -60,7 +60,7 @@ Rails.application.routes.draw do
     end
 
     resources :preferences, only: [:index] do
-      post action: 'update', on: :collection
+      post '', action: 'update', on: :collection
     end
 
     post '/markers' => 'markers#mark'
@@ -81,7 +81,7 @@ Rails.application.routes.draw do
     get '/search/feeds'         => 'feeds#search'
 
     resources :topics, only: [:index, :destroy], constraints: res_options do
-      post action: :update, on: :member
+      post '', action: :update, on: :member
     end
 
     resources :entries, only: [:show], constraints: res_options do
@@ -105,7 +105,7 @@ Rails.application.routes.draw do
     resources :tags, only: [:index], constraints: res_options do
       id_list_regex = /[a-zA-Z0-9\.\,%#\$&\?\(\)\=\+\-\_\:\?\\]+/
       c             = { tag_ids: id_list_regex, entry_ids: id_list_regex }
-      post action: :update, on: :member
+      post '', action: :update, on: :member
       collection do
         put    ':tag_ids'           , action: :tag_entry    , constraints: c
         put    ':tag_ids/:entry_ids', action: :tag_entries  , constraints: c
