1d0ae474922b47908146682cfd96bc646cc72d31
Hiroki Kumamoto <kumabook@live.jp>
2017-03-17 13:42:03 +0900

    Add Enclosure.my_likes_hash and Enclosure.my_saves_hash methods that returns count and my marks

--- app/models/enclosure.rb
+++ app/models/enclosure.rb
@@ -9,6 +9,35 @@ class Enclosure < ApplicationRecord
   scope :latest, -> (time) { where("created_at > ?", time).order('created_at DESC') }
   scope :detail, ->        { eager_load(:users).eager_load(:entries) }
 
+
+  def self.my_likes_hash(user, enclosures)
+    my_likes = LikedEnclosure.where(user_id:      user.id,
+                                    enclosure_id: enclosures.map { |t| t.id })
+    count = LikedEnclosure.where(enclosure_id: enclosures.map { |t| t.id })
+              .group(:enclosure_id).count('enclosure_id')
+    enclosures.inject({}) do |h, t|
+      h[t] = {
+        my: my_likes.to_a.select {|l| t.id == l.enclosure_id }.first,
+        count: count.to_a.select {|c| t.id == c[0] }.map {|c| c[1] }.first,
+      }
+      h
+    end
+  end
+
+  def self.my_saves_hash(user, enclosures)
+    my_saves = SavedEnclosure.where(user_id:      user.id,
+                                    enclosure_id: enclosures.map { |t| t.id })
+    count = SavedEnclosure.where(enclosure_id: enclosures.map { |t| t.id })
+              .group(:enclosure_id).count('enclosure_id')
+    enclosures.inject({}) do |h, t|
+      h[t] = {
+        my: my_saves.to_a.select {|l| t.id == l.enclosure_id }.first,
+        count: count.to_a.select {|c| t.id == c[0] }.map {|c| c[1] }.first,
+      }
+      h
+    end
+  end
+
   def as_content_json
     hash = as_json
     hash['likesCount']   = likes_count
