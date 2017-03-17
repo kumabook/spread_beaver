d0982576d854d8ff3b02f5cfa9c1acaae9f85acd
Hiroki Kumamoto <kumabook@live.jp>
2017-03-18 01:48:49 +0900

    Add like button to Entry index view

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -27,6 +27,8 @@ class EntriesController < ApplicationController
                       .order('published DESC')
                       .page(params[:page])
     end
+    @liked_entries = LikedEntry.where(user_id: current_user.id,
+                                      entry_id: @entries.map { |e| e.id })
     @saved_entries = SavedEntry.where(user_id: current_user.id,
                                       entry_id: @entries.map { |e| e.id })
     @read_entries  = ReadEntry.where(user_id: current_user.id,
