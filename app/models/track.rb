d22c35e3c64916a45d76da3107d6a5ba144399e7
Hiroki Kumamoto <kumabook@live.jp>
2017-03-23 22:32:47 +0900

    Remove unused method of Track

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,18 +1,5 @@
 require 'pink_spider'
 class Track < Enclosure
-  def get_content
-    PinkSpider.new.fetch_track(id)
-  end
-
-  def fetch_content
-    @content = get_content
-  end
-
-  def title
-    fetch_content if @content.nil?
-    "#{@content["title"]} / #{@content["owner_name"]}"
-  end
-
   def permalink_url provider, identifier
     fetch_content if @content.nil?
     @content["url"]
