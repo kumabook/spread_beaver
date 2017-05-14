af14368a83725c4760637cf9f628fb32e9f990ac
Hiroki Kumamoto <kumabook@live.jp>
2017-05-14 22:51:57 +0900

    Create enclosure_issues table

---
+++ db/migrate/20170514072206_create_enclosure_issues.rb
@@ -0,0 +1,12 @@
+class CreateEnclosureIssues < ActiveRecord::Migration[5.0]
+  def change
+    create_table :enclosure_issues do |t|
+      t.string  :enclosure_type, null: false
+      t.uuid    :enclosure_id  , null: false
+      t.uuid    :issue_id      , null: false
+      t.integer :engagement    , null: false, default: 0
+      t.timestamps
+    end
+    add_index :enclosure_issues, [:enclosure_id, :issue_id], unique: true
+  end
+end
