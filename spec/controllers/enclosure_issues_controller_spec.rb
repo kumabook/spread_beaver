e1af1bfa940458eb498514301750b4490e59e4a3
Hiroki Kumamoto <kumabook@live.jp>
2017-05-16 16:42:26 +0900

    Add spec of EnclosureIssuesController

---
+++ spec/controllers/enclosure_issues_controller_spec.rb
@@ -0,0 +1,57 @@
+require 'rails_helper'
+
+describe EnclosureIssuesController, type: :controller do
+  let! (:journal    ) {     Journal.create!(label: "journal", description: "desc")}
+  let! (:issue      ) {       Issue.create!(label: "issue"  , description: "desc", journal_id: journal.id)}
+  let! (:track      ) { FactoryGirl.create (:track                               )}
+  let  (:user       ) { FactoryGirl.create (:admin                               )}
+  let  (:track_issue) { EnclosureIssue.create!(enclosure_id:   track.id,
+                                               enclosure_type: Track.name,
+                                               issue_id: issue.id)}
+
+  before(:each) do
+    login_user user
+  end
+
+  describe 'GET new' do
+    before { get :new, params: { issue_id: issue.id, enclosure_type: Track.name }}
+    it { expect(response).to render_template("new") }
+  end
+
+  describe 'POST create' do
+    before do
+      post :create, params: {
+             enclosure_issue: {
+               issue_id:       issue.id,
+               enclosure_id:   track.id,
+               enclosure_type: Track.name,
+               engagement:     100
+             }
+           }
+    end
+    it { expect(response).to redirect_to issue_tracks_url(issue) }
+    it { expect(EnclosureIssue.find_by(enclosure_id: track.id, issue_id: issue.id)).not_to be_nil }
+  end
+
+  describe 'GET edit' do
+    before do
+      get :edit, params: { id: track_issue.id, issue_id: issue.id }
+    end
+    it { expect(response).to render_template("edit") }
+  end
+
+  describe 'POST update' do
+    before do
+      post :update, params: {
+        id: track_issue.id,
+        issue_id: issue.id,
+        enclosure_issue: {
+          enclosure_type: Track.name,
+          engagement:     200
+        }
+      }
+    end
+    it { expect(response).to redirect_to issue_tracks_url(issue) }
+    it { expect(EnclosureIssue.find(track_issue.id).engagement).to eq(200) }
+  end
+end
