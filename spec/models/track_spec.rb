ddad18daf1e31a586cb84108df842365b210a0aa
Hiroki Kumamoto <kumabook@live.jp>
2017-05-29 18:00:09 +0900

    Add spec of topic scope of Track

--- spec/models/track_spec.rb
+++ spec/models/track_spec.rb
@@ -18,4 +18,20 @@ describe Track do
     end
     it { expect(track.entries.count).to eq(count - 1) }
   end
+
+  describe "::topic" do
+    let (:japanese_feed) { FactoryGirl.create(:feed) }
+    let (:english_feed) { FactoryGirl.create(:feed) }
+    let! (:japanese_topic) {
+      Topic.create!(label: "japanese", description: "desc", feeds: [japanese_feed])
+    }
+    let! (:english_topic) {
+      Topic.create!(label: "english", description: "desc", feeds: [english_feed])
+    }
+
+    TRACKS_PER_TOPIC = TRACK_PER_ENTRY * ENTRY_PER_FEED
+    it { expect(Track.all.count).to eq(TRACKS_PER_TOPIC * 2) }
+    it { expect(Track.topic(japanese_topic).count).to eq(TRACKS_PER_TOPIC) }
+    it { expect(Track.topic(english_topic).count).to eq(TRACKS_PER_TOPIC) }
+  end
 end
