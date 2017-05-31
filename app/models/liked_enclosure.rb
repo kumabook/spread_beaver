74ed26aad77a1a03b420d0142397037a515c09b4
Hiroki Kumamoto <kumabook@live.jp>
2017-05-31 10:58:56 +0900

    LikedEnclosure and PlayedEnclosure include EnclosureMark

--- app/models/liked_enclosure.rb
+++ app/models/liked_enclosure.rb
@@ -1,7 +1,5 @@
 class LikedEnclosure < ApplicationRecord
+  include EnclosureMark
   belongs_to :user
   belongs_to :enclosure, counter_cache: :likes_count, touch: true
-
-  scope :period,     -> (from, to) { where("created_at >= ?", from).where("created_at <= ?", to) }
-  scope :user_count, ->            { group(:enclosure_id).order('count_user_id DESC').count('user_id') }
 end
