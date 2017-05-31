27896513ed5c0e9c2db175a319b882f94f2071b7
Hiroki Kumamoto <kumabook@live.jp>
2017-05-31 10:58:17 +0900

    LikedEntry and ReadEntry models include EntryMark

--- app/models/liked_entry.rb
+++ app/models/liked_entry.rb
@@ -1,7 +1,5 @@
 class LikedEntry < ApplicationRecord
+  include EntryMark
   belongs_to :user
   belongs_to :entry, counter_cache: :likes_count, touch: true
-
-  scope :period,     -> (from, to) { where("created_at >= ?", from).where("created_at <= ?", to) }
-  scope :user_count, ->            { group(:entry_id).order('count_user_id DESC').count('user_id') }
 end
