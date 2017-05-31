42e4b3911964be2e451c8d8375bb141e65d4cf35
Hiroki Kumamoto <kumabook@live.jp>
2017-05-31 12:43:21 +0900

    Add V3::MixesController

---
+++ app/controllers/v3/mixes_controller.rb
@@ -0,0 +1,43 @@
+class V3::MixesController < V3::ApiController
+  include Pagination
+  include V3::StreamsControllable
+  if Rails.env.production?
+    before_action :doorkeeper_authorize!
+  end
+  before_action :set_mix_type
+  before_action :set_stream
+  before_action :set_period
+  before_action :set_items
+
+  def index
+    continuation = self.class.calculate_continuation(@items, @page, @per_page)
+    Entry.set_contents_of_enclosures(@items)
+    if current_resource_owner.present?
+      Entry.set_marks(current_resource_owner, @items)
+      Entry.set_marks_of_enclosures(current_resource_owner, @items)
+    end
+    h = {
+      direction:    "ltr",
+      continuation: continuation,
+      alternate:    [],
+      items:        @items,
+    }
+    if @feed.present?
+      h[:updated] = @feed.updated_at.to_time.to_i * 1000
+      h[:title]   = @feed.title
+    end
+    render json: h, status: 200
+  end
+
+  def set_items
+    if @stream.present?
+      query  = Mix::Query.new(@period, @type, entries_per_feed: entries_per_feed)
+      @items = @stream.mix_entries(page: @page, per_page: @per_page, query: query)
+    end
+
+    if @items.nil?
+      render json: {message: "Not found" }, status: 404
+      return
+    end
+  end
+end
