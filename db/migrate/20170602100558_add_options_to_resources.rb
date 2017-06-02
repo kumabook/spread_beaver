dc2f1eec4d315ed3d7478594ca1d8e604459590a
Hiroki Kumamoto <kumabook@live.jp>
2017-06-02 20:20:41 +0900

    Add options column to resources table

---
+++ db/migrate/20170602100558_add_options_to_resources.rb
@@ -0,0 +1,5 @@
+class AddOptionsToResources < ActiveRecord::Migration[5.0]
+  def change
+    add_column :resources, :options, :text
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170514130008) do
+ActiveRecord::Schema.define(version: 20170602100558) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -267,6 +267,7 @@ ActiveRecord::Schema.define(version: 20170514130008) do
     t.integer  "engagement",    default: 0, null: false
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
+    t.text     "options"
   end
 
   create_table "saved_enclosures", force: :cascade do |t|
