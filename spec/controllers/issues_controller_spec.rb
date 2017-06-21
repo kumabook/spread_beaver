a0277fa27174f75039d46cf4573ffba74369bfea
Hiroki Kumamoto <kumabook@live.jp>
2017-06-21 10:47:54 +0900

    Add spec of IssueController#create_daily_issue and #collect_entries

--- spec/controllers/issues_controller_spec.rb
+++ spec/controllers/issues_controller_spec.rb
@@ -1,8 +1,9 @@
 require 'rails_helper'
 
 describe IssuesController, type: :controller do
-  let! (:journal) {     Journal.create!(label: "journal", description: "desc")}
-  let! (:issue  ) {       Issue.create!(label: "issue"  , description: "desc", journal_id: journal.id)}
+  let! (:topic) {   Topic.create!(label: "highlight", description: "desc")}
+  let! (:journal) { Journal.create!(label: "highlight", description: "desc")}
+  let! (:issue  ) { Issue.create!(label: "20170407", description: "desc", journal_id: journal.id)}
   let  (:user   ) { FactoryGirl.create (:admin                               )}
 
   before(:each) do
@@ -48,5 +49,35 @@ describe IssuesController, type: :controller do
     before { delete :destroy, params: { id: issue.id, journal_id: journal.id }}
     it { expect(response).to redirect_to journal_issues_url(journal) }
     it { expect(Issue.find_by(id: issue.id)).to be_nil }
+  end
+
+  describe "POST create_daily" do
+    before {
+      post :create_daily, params: {
+             journal_id: journal.id,
+           }
+    }
+    it { expect(response).to redirect_to journal_issues_url(journal) }
+    it { expect(journal.issues.count).to eq(2) }
+  end
+
+  describe "POST collect_entries" do
+    before {
+      feed  = Feed.create(id: "feed/http://test.com", title: "")
+      entry = FactoryGirl.create(:normal_entry, feed: feed)
+      entry.feed.topics = [topic]
+      entry.update!(published: Time.zone.strptime("20170407", "%Y%m%d"))
+
+      entry2 = FactoryGirl.create(:normal_entry, feed: feed)
+      entry2.feed.topics = [topic]
+      entry2.update!(published: Time.zone.strptime("20170409", "%Y%m%d"))
+
+      post :collect_entries, params: {
+             journal_id: journal.id,
+             id:         issue.id,
+           }
+    }
+    it { expect(response).to redirect_to edit_journal_issue_url(journal, issue) }
+    it { expect(issue.entries.count).to eq(1) }
   end
 end
