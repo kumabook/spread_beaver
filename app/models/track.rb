802357945b10620348a2ed29f2a90390377ad4f6
Hiroki Kumamoto <kumabook@live.jp>
2017-11-17 20:00:45 +0900

    Refactor Track#permalink_url

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,7 +1,13 @@
 require 'pink_spider'
 class Track < Enclosure
-  def permalink_url provider, identifier
+  def permalink_url
     fetch_content if @content.nil?
-    @content["url"]
+    case @content["provider"]
+    when 'Spotify'
+      s = @content["url"].split(':')
+      "http://open.spotify.com/#{s[1]}/#{s[2]}"
+    else
+      @content["url"]
+    end
   end
 end
