ec5a8c58410a09d5749d03879985177d16f553ca
Hiroki Kumamoto <kumabook@live.jp>
2018-01-06 06:45:48 +0900

    Define `set_track_properties` task

--- db/migrate/20171117113022_add_provider_to_enclosure.rb
+++ db/migrate/20171117113022_add_provider_to_enclosure.rb
@@ -6,35 +6,6 @@ class AddProviderToEnclosure < ActiveRecord::Migration[5.0]
     add_column :enclosures, :title, :string, default: ""
     add_index :enclosures, :title, unique: false
     add_index :enclosures, :provider, unique: false
-
-    batch_size = 100
-    pink_spider = PinkSpider.new
-    Track.all.find_in_batches(batch_size: batch_size) do |items|
-      tracks = pink_spider.fetch_tracks(items.map {|i| i.id })
-      items.each do |item|
-        t = tracks.find {|track| track["id"] == item.id }
-        item.update_columns(title: t["title"], provider: t["provider"])
-        p "update #{t["title"]}"
-      end
-    end
-
-    Album.all.find_in_batches(batch_size: batch_size) do |items|
-      albums = pink_spider.fetch_albums(items.map {|i| i.id })
-      items.each do |item|
-        a = albums.find {|album| album["id"] == item.id }
-        item.update_columns(title: a["title"], provider: a["provider"])
-        p "update #{a["title"]}"
-      end
-    end
-
-    Playlist.all.find_in_batches(batch_size: batch_size) do |items|
-      playlists = pink_spider.fetch_playlists(items.map {|i| i.id })
-      items.each do |item|
-        pl = playlists.find {|playlist| playlist["id"] == item.id }
-        item.update_columns(title:  pl["title"], provider: pl["provider"])
-        p "update #{pl["title"]}"
-      end
-    end
   end
 
   def down
---
+++ lib/tasks/set_track_properties.rake
@@ -0,0 +1,32 @@
+require "pink_spider"
+
+task :set_track_properties => :environment do
+  batch_size = 100
+  pink_spider = PinkSpider.new
+  Track.where(provider: nil).find_in_batches(batch_size: batch_size) do |items|
+    tracks = pink_spider.fetch_tracks(items.map {|i| i.id })
+    items.each do |item|
+      t = tracks.find {|track| track["id"] == item.id }
+      item.update_columns(title: t["title"], provider: t["provider"])
+      puts "update track #{t["title"]} #{t["id"]}"
+    end
+  end
+
+  Album.where(provider: nil).find_in_batches(batch_size: batch_size) do |items|
+    albums = pink_spider.fetch_albums(items.map {|i| i.id })
+    items.each do |item|
+      a = albums.find {|album| album["id"] == item.id }
+      item.update_columns(title: a["title"], provider: a["provider"])
+      puts "update album #{a["title"]} #{a["id"]}"
+    end
+  end
+
+  Playlist.where(provider: nil).find_in_batches(batch_size: batch_size) do |items|
+    playlists = pink_spider.fetch_playlists(items.map {|i| i.id })
+    items.each do |item|
+      pl = playlists.find {|playlist| playlist["id"] == item.id }
+      item.update_columns(title:  pl["title"], provider: pl["provider"])
+      puts "update playlist #{pl["title"]} #{pl["id"]}"
+    end
+  end
+end
