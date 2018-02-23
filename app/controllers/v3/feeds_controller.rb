81b4f59365ee0662c1e2f601dd90cadcca19991e
Hiroki Kumamoto <kumabook@live.jp>
2018-02-23 15:51:27 +0900

    feed api fastly cache

--- app/controllers/v3/feeds_controller.rb
+++ app/controllers/v3/feeds_controller.rb
@@ -2,6 +2,7 @@ class V3::FeedsController < V3::ApiController
 #  before_action :doorkeeper_authorize!
   before_action :set_feed,  only: [:show]
   before_action :set_feeds, only: [:list]
+  before_action :set_cache_control_headers, only: [:search, :show, :list]
 
   def search
     @feeds = Feed.search_by(query: search_params[:query],
@@ -13,11 +14,13 @@ class V3::FeedsController < V3::ApiController
          hint: "",
       results: @feeds
     }
+    set_surrogate_key_header Feed.table_key, @feeds.map(&:record_key)
     render json: result, status: 200
   end
 
   def show
     if @feed.present?
+      set_surrogate_key_header @feed.record_key
       render json: @feed.to_json, status: 200
     else
       render_not_found
@@ -27,6 +30,7 @@ class V3::FeedsController < V3::ApiController
 
   def list
     if @feeds.present?
+      set_surrogate_key_header Feed.table_key, @feeds.map(&:record_key)
       render json: @feeds.to_json, status: 200
     else
       render_not_found
