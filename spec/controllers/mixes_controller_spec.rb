7ac2b6d2d31480ca66f1334f50467d5df4d4c119
Hiroki Kumamoto <kumabook@live.jp>
2018-03-29 20:19:19 +0900

    Add spec of mixes_controller

---
+++ spec/controllers/mixes_controller_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+describe MixesController, type: :controller do
+  let  (:user) { FactoryBot.create(:admin) }
+  before(:all) do
+    @feed  = FactoryBot.create(:feed)
+    @topic = FactoryBot.create(:topic)
+  end
+  before(:each) do
+    login_user user
+  end
+
+  describe '#index' do
+    before { get :index }
+    it { expect(response).to render_template("index") }
+  end
+
+  describe '#show' do
+    before {
+      get :show,
+          params: {
+            newerThan:  7.days.ago.to_time.to_i * 1000,
+            period:     "weekly",
+            type:       :hot,
+            id:         CGI.escape(@topic.id),
+          }
+    }
+    it { expect(assigns(:items)).not_to be_nil }
+    it { expect(response).to render_template("show") }
+  end
+
+end
