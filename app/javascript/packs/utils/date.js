def7a41202188dacba4bf6538ab94faab548bbc3
Hiroki Kumamoto <kumabook@live.jp>
2018-04-13 17:35:57 +0900

    Update mixes index view: Set olderThan parameter with midnight of today

---
+++ app/javascript/packs/utils/date.js
@@ -0,0 +1,22 @@
+import moment from 'moment';
+
+export function since(d) {
+  return moment()
+    .subtract(d, 'days')
+    .utcOffset('+09:00')
+    .startOf('day')
+    .valueOf();
+}
+
+export function fromToday(type) {
+  switch (type) {
+    case 'daily':
+      return { newerThan: since(1), olderThan: since(0) };
+    case 'weekly':
+      return { newerThan: since(7), olderThan: since(0) };
+    case 'monthly':
+      return { newerThan: since(30), olderThan: since(0) };
+    default:
+      return { newerThan: since(7), olderThan: since(0) };
+  }
+}
