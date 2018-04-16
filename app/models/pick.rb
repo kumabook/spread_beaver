e398dd9605613f825df3c0713c732761505c99b0
Hiroki Kumamoto <kumabook@live.jp>
2018-04-16 10:41:04 +0900

    Customize Pick stream scopes: Join enclosures table on `container_id`

--- app/models/pick.rb
+++ app/models/pick.rb
@@ -7,4 +7,25 @@ class Pick < ApplicationRecord
     group(:enclosure_id).order('count_container_id DESC').count('container_id')
   }
 
+  scope :feed, -> (feed) {
+    joins(container: :entries).where(entries: { feed_id: feed.id })
+  }
+  scope :keyword, -> (keyword) {
+    joins(container: { entries: :keywords })
+      .where(keywords: { id: keyword.id })
+  }
+  scope :tag, -> (tag) {
+    joins(container: { entries: :tags }).where(tags: { id: tag.id })
+  }
+  scope :topic,      -> (topic)    {
+    joins(container: { entries: { feed: :topics }})
+      .where(topics: { id: topic.id })
+  }
+  scope :category, -> (category) {
+    joins(container: { entries: { feed: { subscriptions: :categories }}})
+      .where(categories: { id: category.id })
+  }
+  scope :issue, -> (issue) {
+    joins(container: { entries: :issues }).where(issues: { id: issue.id })
+  }
 end
