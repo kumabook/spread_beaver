33a2eac319a16af2ce270b77505aa112142c9885
Hiroki Kumamoto <kumabook@live.jp>
2018-04-20 15:17:56 +0900

    Add locale and use_stream_for_pick option to mix index view

--- app/javascript/packs/mixes/index.js
+++ app/javascript/packs/mixes/index.js
@@ -17,17 +17,20 @@ function providers(value) {
   }
 }
 
-function mixParams(type, period, provider) {
+function mixParams(type, locale, period, provider, use_stream_for_pick) {
   const { newerThan, olderThan } = fromToday(period);
   return {
     type,
+    locale,
     newerThan,
     olderThan,
     period,
-    provider
+    provider,
+    use_stream_for_pick
   };
 }
 
+var locale     = document.getElementById("locale");
 var streamId   = document.getElementById("stream_id");
 var streamType = document.getElementById("stream_type");
 var mixType    = document.getElementById("mix_type");
@@ -35,12 +38,16 @@ var period     = document.getElementById("period");
 var provider   = document.getElementById("provider");
 var mixForm    = document.getElementById("mix_form");
 
+var use_stream_for_pick = document.getElementById("use_stream_for_pick");
+
 mixForm.addEventListener('submit', function(e) {
   e.preventDefault();
   var url   = mixUrl(streamId.value, streamType.value);
   var params = mixParams(mixType.value,
+                         locale.value,
                          period.value,
-                         providers(provider.value));
+                         providers(provider.value),
+                         use_stream_for_pick.value);
   var query = qs.stringify(params, { arrayFormat: 'brackets' });
   document.location.href = `${url}?${query}`;
 });
