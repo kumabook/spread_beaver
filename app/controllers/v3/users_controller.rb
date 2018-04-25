7b67c27c5f3c3ae86fb354266ebbee88cd2c85e3
Hiroki Kumamoto <kumabook@live.jp>
2018-04-25 16:00:34 +0900

    Don't use redundant merge

--- app/controllers/v3/users_controller.rb
+++ app/controllers/v3/users_controller.rb
@@ -55,7 +55,7 @@ class V3::UsersController < V3::ApiController
 
   def profile_params
     h = params.permit(:email, :name, :fullName, :locale, :twitter_user_id, :profile, :picture)
-    h.merge!({ name: h[:fullName] }) if h.has_key?(:fullName)
+    h[:name] = h[:fullName] if h.has_key?(:fullName)
     h.delete(:fullName)
     h
   end
