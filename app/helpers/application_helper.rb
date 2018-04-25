074ba9a7961d429b21d7a1ab85f8f3ba5b2a65e0
Hiroki Kumamoto <kumabook@live.jp>
2018-04-25 16:11:52 +0900

    Enable `Style/MethodDefParentheses` and autocorrect with it

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 module ApplicationHelper
-  def bootstrap_class_for flash_type
+  def bootstrap_class_for(flash_type)
     { success: "alert-success", error: "alert-danger", alert: "alert-warning", notice: "alert-info" }[flash_type.to_sym] || flash_type.to_s
   end
 
