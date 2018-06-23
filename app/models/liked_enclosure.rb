8c253975ed4f7fb311bf4ebca2a8f42210eaaddd
Hiroki Kumamoto <kumabook@live.jp>
2018-06-23 19:04:27 +0900

    Liked|Saved|PlayedEnclosure has polymorphic association for tracks or albums or playlists. And they have each association for mix query

--- app/models/liked_enclosure.rb
+++ app/models/liked_enclosure.rb
@@ -5,5 +5,9 @@ class LikedEnclosure < ApplicationRecord
   include_stream_scopes
 
   belongs_to :user
-  belongs_to :enclosure, counter_cache: :likes_count, touch: true
+  belongs_to :enclosure, counter_cache: :likes_count, touch: true, polymorphic: true
+
+  belongs_to :track   , foreign_key: "enclosure_id", required: false
+  belongs_to :album   , foreign_key: "enclosure_id", required: false
+  belongs_to :playlist, foreign_key: "enclosure_id", required: false
 end
--- app/models/saved_enclosure.rb
+++ app/models/saved_enclosure.rb
@@ -5,5 +5,9 @@ class SavedEnclosure < ApplicationRecord
   include_stream_scopes
 
   belongs_to :user
-  belongs_to :enclosure, counter_cache: :saved_count, touch: true
+  belongs_to :enclosure, counter_cache: :saved_count, touch: true, polymorphic: true
+
+  belongs_to :track   , foreign_key: "enclosure_id", required: false
+  belongs_to :album   , foreign_key: "enclosure_id", required: false
+  belongs_to :playlist, foreign_key: "enclosure_id", required: false
 end
