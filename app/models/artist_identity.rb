62736469be8e6da4d9c6d1574c025091f45d834e
Hiroki Kumamoto <kumabook@live.jp>
2018-07-09 16:07:15 +0900

    Implement ArtistIdentity

---
+++ app/models/artist_identity.rb
@@ -0,0 +1,117 @@
+# frozen_string_literal: true
+
+class ArtistIdentity < ApplicationRecord
+  include Enclosure
+  include Identity
+  has_many :items, class_name: "Artist", foreign_key: :identity_id
+  has_many :track_artist_identities
+  has_many :album_artist_identities
+  has_many :track_identities, through: :track_artist_identities
+  has_many :album_identities, through: :album_artist_identities
+
+  def self.find_or_create_by_artist(artist)
+    case artist.provider
+    when "Spotify"
+      a = RSpotify::Artist.find(artist.identifier)
+      find_or_create_by_spotify_album(t)
+    when "AppleMusic"
+      s = AppleMusic::Artist.find("jp", album.identifier)
+      find_or_create_by_apple_music_album(s)
+    end
+  end
+
+  def self.find_or_create_by_spotify_artist(a)
+    find_or_create_by(name: a.name, origin_name: "")
+  end
+
+  def self.find_or_create_by_apple_music_artist(a)
+    find_or_create_by(name: a.name, origin_name: "")
+  end
+
+  def self.build_by_spotify_artist(artist)
+    return nil if artist.nil?
+    item = Artist.find_or_create_by_spotify_artist(artist)
+    return item.identity if item.identity.present?
+    identity = find_or_create_by_spotify_artist(artist)
+    identity.update_associations_by_spotify_artist(artist)
+    identity.search_apple_music
+    identity
+  end
+
+  def self.build_by_apple_music_artist(artist)
+    return nil if artist.nil?
+    item = Artist.find_or_create_by_apple_music_artist(artist)
+    return item.identity if item.identity.present?
+    identity = find_or_create_by_apple_music_artist(artist)
+    identity.update_associations_by_apple_music_artist(artist)
+    identity.search_spotify
+    identity
+  end
+
+  def update_associations_by_spotify_artist(artist)
+    item = Artist.find_or_create_by_spotify_artist(artist)
+    artist.genres.each do |genre|
+      p genre
+#      genre_tag = Tag::find_or_create_by(genre, "genre")
+#      add_tag(genre_tag)
+    end
+    artist_identity = ArtistIdentity::find_by(id: item.identity_id)
+    if artist_identity.present? && id != artist_identity.id
+      identity.merge_to(self)
+      identity.destroy
+    end
+    item.identity = self
+    item.save!
+    ArtistAlias.find_or_create_by(artist_identity_id: id, name: artist.name)
+    self
+  end
+
+  def update_associations_by_apple_music_artist(artist)
+    item = Artist::find_or_create_by_apple_music_artist(artist)
+    artist.genre_names.each do |genre|
+      p genre
+#      genre_tag = Tag.find_or_create_by(genre.name, "genre")
+#      add_tag(genre_tag)
+    end
+    item.identity = self
+    item.save!
+    ArtistAlias.find_or_create_by(artist_identity_id: id, name: artist.name)
+    self
+  end
+
+  def merge_to(another)
+    AlbumArtistIdentity.where(artist_identity_id: id).update_all(artist_identity_id: another.id)
+    TrackArtistIdentity.where(artist_identity_id: id).update_all(artist_identity_id: another.id)
+    Artist.where(identity_id: id).update_all(identity_id: another.id)
+    ArtistAlias.where(artist_identity_id: id).update_all(artist_identity_id: another.id)
+  end
+
+  def search_apple_music
+    artists = AppleMusic::Artist.search("jp", [name])
+    return if artists.blank?
+
+    AppleMusic::Artist.find("jp", artists.map(&:id)).each do |a|
+      if artists.count == 1 || a.name == name
+        artist = Artist.find_or_create_by_apple_music_artist(a)
+        artist.identity = self
+        artist.save!
+      end
+    end
+  end
+
+  def search_spotify
+    artists = RSpotify::Artist.search("artist:#{name}", limit: 10)
+    artists.each do |artist|
+      if artists.count == 1 || artist.name == name
+        artist = Artist.find_or_create_by_spotify_artist(artist)
+        artist.identity = self
+        artist.save!
+      end
+    end
+  end
+
+  def search_items
+    search_apple_music
+    search_spotify
+  end
+end
--- db/migrate/20180625082002_create_artist_identities.rb
+++ db/migrate/20180625082002_create_artist_identities.rb
@@ -5,6 +5,14 @@ class CreateArtistIdentities < ActiveRecord::Migration[5.1]
     create_table :artist_identities, id: :uuid, default: "uuid_generate_v4()" do |t|
       t.string :name, null: false
       t.string :origin_name, null: false
+
+      t.integer :entries_count, default: 0, null: false
+
+      t.integer :likes_count, default: 0, null: false
+      t.integer :saved_count, default: 0, null: false
+      t.integer :play_count, default: 0, null: false
+      t.timestamps null: false
+
       t.index [:name]
     end
     create_table :track_artist_identities do |t|
